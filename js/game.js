e391fae7d39eed247276ee85b9a9a4c88d7cb8b1
George Skrapits & Susan Yi <pair+gskrap.yisusans@devbootcamp.com>
2016-06-20 15:20:44 -0400

    up moves work

--- js/game.js
+++ js/game.js
@@ -14,9 +14,13 @@ $(document).on('keyup',function(e){
 });
 
 
-function Game(starting_board) {
-  this.starting_board = starting_board || boardGenerator();
-  this.current_board = this.starting_board
+String.prototype.replaceAt=function(index, character) {
+  return this.substr(0, index) + character + this.substr(index+character.length);
+}
+
+function Game(startingBoard) {
+  this.startingBoard = startingBoard || boardGenerator();
+  this.currentBoard = this.startingBoard
 }
 
 function boardGenerator() {
@@ -28,4 +32,39 @@ function boardGenerator() {
     else { result += "0"; }
   }
   return result;
+}
+
+Game.prototype.spawnBlock = function() {
+  board = this.currentBoard;
+  var idx = "";
+  var newBoard = "";
+  while (idx == "") {
+    idx = Math.floor(Math.random() * 16);
+    if (board[idx] != 0){ idx = ""};
+  }
+  for (var i=0; i < 16; i++) {
+    if (i == idx)  { newBoard += "2"; }
+    else { newBoard += board[i];}
+  }
+  this.currentBoard = newBoard;
+}
+
+Game.prototype.moveUp = function() {
+  for (var i = 0; i < 16; i++) {
+    for (var j = 0; j < 16; j++) {
+      if ((j % 4 == i % 4) && ((j/4)-(i/4) == -1) && (i != j) && this.currentBoard[j] != "0") {
+        this.currentBoard = this.currentBoard.replaceAt(j, (String(Number(this.currentBoard[j]) * 2)));
+        this.currentBoard = this.currentBoard.replaceAt(i, "0");
+      }
+    }
+  }
+}
+
+Game.prototype.clearUp = function() {
+  for (var i = 0; i < 16; i++) {
+    if ((i/4 < 3) && (this.currentBoard[i] == 0) && (this.currentBoard[i+4] != 0)) {
+      this.currentBoard = this.currentBoard.replaceAt(i, this.currentBoard[i+4])
+      this.currentBoard = this.currentBoard.replaceAt((i+4), "0")
+    }
+  }
 }
